3afcda8ed4b6dcfa5393f43e0b2a7d1c3392a57f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-30 20:03:45 +0300

    update

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ const dashs = document.querySelector(".dashs");
 let curSlide = 0;
 const maxSlide = slides.length; //Number of Nodelist
 
-//Functions 
+//Functions
 const createDashs = () => {
-    slides.forEach(function(_, i){
-        dashs.insertAdjacentHTML(
-          "beforeend",
-          `
+  slides.forEach(function (_, i) {
+    dashs.insertAdjacentHTML(
+      "beforeend",
+      `
             <button class="dashs-dash" data-slide="${i}"></button>
         `
-        );
-    });
+    );
+  });
 };
 createDashs();
 
@@ -42,17 +42,17 @@ goToSlide(0);
 
 //Next slide
 const nextSlide = () => {
-    curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
-    goToSlide(curSlide);
-    activeDash(curSlide);
-}
+  curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+  goToSlide(curSlide);
+  activeDash(curSlide);
+};
 
 //Prev slide
 const prevSlide = () => {
-    curSlide === 0 ? curSlide = maxSlide - 1 : curSlide--;
-    goToSlide(curSlide);
-    activeDash(curSlide);
-}
+  curSlide === 0 ? (curSlide = maxSlide - 1) : curSlide--;
+  goToSlide(curSlide);
+  activeDash(curSlide);
+};
 
 setInterval(nextSlide, 3000);
 
@@ -67,3 +67,39 @@ dashs.addEventListener("click", (e) => {
     activeDash(slide);
   }
 });
+
+const loadData = () => {
+  const mainContentDiv = document.querySelector("#mainContent");
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((json) =>
+      json.products.map((el) => {
+        console.log(el);
+        mainContentDiv.innerHTML += `
+        <div class="card border-0 shadow mb-5" style="width: 18rem">
+          <img
+            src="${el.images[0]}"
+            class="border-0 p-2"
+            alt=""
+            style="height: 15rem !important"
+          />
+          <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">${el.title}</h5>
+            <p class="card-text">
+          ${el.description}
+            </p>
+            <div class="text-center fw-bold link-danger mb-2">${el.price} $</div>
+            <div class="d-flex justify-content-between mt-auto">
+              <a href="" class="btn btn-warning">
+                View More
+              </a>
+              <button class="btn btn-success">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      `;
+      })
+    );
+};
+
+loadData();
