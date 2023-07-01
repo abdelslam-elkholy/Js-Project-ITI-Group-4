b62067b5540a4335bba6441c056beb3b1782df29
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-02 00:48:48 +0300

    update

--- script.js
+++ script.js
@@ -69,52 +69,89 @@ dashs.addEventListener("click", (e) => {
 });
 
 // Function to Get Data From API and Append It to Main
-const loadData = () => {
+
+const loadData = (param) => {
+  // load Categories Menue
+  getCategories();
+
+  // Main Content Div
   const mainContentDiv = document.querySelector("#mainContent");
-  fetch("https://dummyjson.com/products?limit=0")
+  mainContentDiv.innerHTML = "";
+
+  //Get Data From API
+  fetch(`https://dummyjson.com/${param}`)
     .then((res) => res.json())
-    .then((json) =>
-      json.products.map((el) => {
-        console.log(el);
+    .then((json) => {
+      let res = json.products;
+      console.log(res);
+      //Loop Over Data And MAke A Card For Each Product
+      res.map((el) => {
+        // console.log(el);
         mainContentDiv.innerHTML += `
         <div class="card border-0 shadow mb-5" style="width: 18rem">
-          <img
-            loading="lazy"
-            src="${el.thumbnail}"
-            class="border-0 p-2"
-            alt=""
-            style="height: 15rem !important"
-          />
-          <div class="card-body d-flex flex-column justify-content-between">
-            <h5 class="card-title" style="cursor:pointer" onclick="openSingleProductPage(${
-              el.id
-            })"> 
-           
-            
-            ${el.title}
-              
+        <img
+        loading="lazy"
+        src="${el.thumbnail}"
+        class="border-0 p-2"
+        alt=""
+        style="height: 15rem !important"
+        />
+        <div class="card-body d-flex flex-column justify-content-between">
+        <h5 class="card-title" style="cursor:pointer" onclick="openSingleProductPage(${
+          el.id
+        })"> 
+        ${el.title}  
         </h5>
-            <p class="card-text " style = "height : 6rem !important">
-          ${
-            el.description.length > 100
-              ? el.description.slice(0, 100) + "..."
-              : el.description
-          }
-            </p>
-            <div class=" fw-bold  m-3">${el.price} $</div>
-            <div class="d-flex justify-content-between mt-auto">
-              <button href="" class="btn btn-warning"  >
-                Add To Cart
-              </button>
-              <span class=" bg-success link-light p-2 rounded">
-              <i class="fa-solid fa-star"></i>
-              ${el.rating.toFixed(1)}</span>
-            </div>
-            
-          </div>
+        <p class="card-text " style = "height : 6rem !important">
+        ${
+          el.description.length > 100
+            ? el.description.slice(0, 100) + "..."
+            : el.description
+        }
+        </p>
+        <div class=" fw-bold  m-3">${el.price} $</div>
+        <div class="d-flex justify-content-between mt-auto">
+        <button href="" class="btn btn-warning"  >
+        Add To Cart
+        </button>
+        <span class=" bg-success link-light p-2 rounded">
+        <i class="fa-solid fa-star"></i>
+        ${el.rating.toFixed(1)}</span>
         </div>
-      `;
-      })
+        
+        </div>
+        </div>
+        `;
+      });
+    });
+};
+
+// Get All Categories
+const getCategories = () => {
+  const categoriesContainer = document.querySelector(".categories");
+  categoriesContainer.innerHTML = `
+        <li class="nav-item">
+         <a class="nav-link 
+          text-dark" href="#"
+          onclick="loadData('products?limit=0')"
+          >
+          ALL CATOGARIES &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+         </a>
+        </li>`;
+
+  fetch("https://dummyjson.com/products/categories")
+    .then((res) => res.json())
+    .then((json) =>
+      json.map(
+        (cat) =>
+          (categoriesContainer.innerHTML += ` 
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#" 
+              onclick="loadData('products/category/${cat}')">
+              ${cat}
+              </a>
+           </li>`)
+      )
     );
 };
 
@@ -126,19 +163,4 @@ const openSingleProductPage = (id) => {
 };
 
 // Fire The Function When Load
-loadData();
-
-/*
-
-`
-        <div class="item">
-          <img src="${el.thumbnail}" alt="el.title" class="item-img" />
-          <div class="item-name">${el.title}</div>
-          <div class="item-details">
-            <span class="item-spec">Price</span>
-            <span class="item-price">${el.price}$</span>
-          </div>
-          <button class="item-add btn btn-warning">Add To Cart</button>
-        </div>`;
-      })
-*/
+loadData("products?limit=0");
