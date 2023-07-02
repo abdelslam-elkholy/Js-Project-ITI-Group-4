984c67b8127eff69fed49244ac731c7373f1d9db
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-02 17:06:49 +0300

    update

--- script.js
+++ script.js
@@ -1,10 +1,218 @@
 "use strict";
 
+// ----------- Forms -------------
+
+const modal = document.querySelector(".wrapper");
+const btnLoginModal = document.querySelector(".login-link");
+const btnRegisterModal = document.querySelector(".register-link");
+const overlay = document.querySelector(".overlay");
+const btnOpen = document.querySelector(".open");
+const btnClose = document.querySelector(".icon-close");
+const inputBoxs = document.querySelectorAll(".input-box");
+const loginForm = document.querySelector(".login-form");
+const registrationForm = document.querySelector(".register-form");
+const errorContainer = document.getElementById("errorContainer");
+const successContainer = document.getElementById("successContainer");
+
+const isValidEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+};
+
+const isValidPassword = (password) => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+  return passwordRegex.test(password);
+};
+
+const isValidName = (userName) => {
+  const usernameRegex = /^[a-zA-Z0-9]+$/;
+  return usernameRegex.test(userName);
+};
+
+const displayError = (errorMessage) => {
+  errorContainer.style.display = "block";
+  errorContainer.style.color = "red";
+  errorContainer.textContent = errorMessage;
+};
+const displaySuccess = (successMessage) => {
+  successContainer.style.display = "block";
+  successContainer.style.color = "green";
+  successContainer.textContent = successMessage;
+};
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("user").value;
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  console.log(typeof users);
+
+  // const user = users.find(
+  //   (user) => user.username === username && user.password === password
+  // );
+  let user = null;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      user = users[i];
+      break;
+    }
+  }
+
+  if (user) {
+    localStorage.setItem("loggedInUser", JSON.stringify(user));
+    console.log("Login successful");
+  } else {
+    console.log("Invaild");
+  }
+
+  // if (email === "") {
+  //   displayError("Email field is required!");
+  //   return;
+  // }
+
+  // // Perform additional validation on email format
+  // if (!isValidEmail(email)) {
+  //   displayError("Invalid email address!");
+  //   return;
+
+  //   // alert("Invalid email address!");
+  //   // return;
+  // }
+
+  // if (password === "") {
+  //   displayError("Password field is required!");
+  //   return;
+  //   // alert("Password field is required!");
+  //   // return;
+  // }
+
+  // if (!isValidPassword(password)) {
+  //   displayError("Invalid password!");
+  //   //alert("Invalid password!");
+  //   return;
+  // }
+
+  // if (username === "") {
+  //   displayError("Name field is required!");
+  //   //alert("Name field is required!");
+  //   return;
+  // }
+
+  // if (!isValidName(username)) {
+  //   displayError("Invalid name!");
+  //   //alert("Invalid name!");
+  //   return;
+  // }
+
+  // If all validations pass, submit the form
+  alert("Form submitted successfully!");
+  loginForm.submit();
+});
+
+registrationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const regEmail = document.getElementById("email1").value;
+  const regPassword = document.getElementById("password1").value;
+  const regUserName = document.getElementById("name1").value;
+
+  if (regEmail === "") {
+    displayError("Email field is required!");
+    return;
+  }
+
+  if (!isValidEmail(regEmail)) {
+    displayError("Invalid email address!");
+    return;
+  }
+
+  if (regPassword === "") {
+    displayError("Password field is required!");
+    return;
+  }
+
+  if (!isValidPassword(regPassword)) {
+    displayError("Invalid password!");
+    return;
+  }
+
+  if (regUserName === "") {
+    displayError("Name field is required!");
+    return;
+  }
+
+  if (!isValidName(regUserName)) {
+    displayError("Invalid name!");
+    return;
+  }
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  // const isUserName = users.some((user) => user.userName === regUserName);
+  let isUserName = false;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === regUserName) {
+      isUserName = true;
+      break;
+    }
+  }
+  if (isUserName) {
+    console.log("Username already logged in");
+  } else {
+    const newUser = {
+      username: regUserName,
+      password: regPassword,
+    };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    console.log("Registration successful");
+  }
+
+  //alert("Form submitted successfully!");
+  displaySuccess("Form submitted successfully!");
+  registrationForm.submit();
+});
+
+btnRegisterModal.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+btnLoginModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+const openModal = () => {
+  modal.classList.add("active-btn");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  modal.classList.remove("active-btn");
+  overlay.classList.add("hidden");
+};
+
+btnOpen.addEventListener("click", openModal);
+
+btnClose.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+
+// ----------- Slider -------------
+
+const imgNoon = document.querySelector(".img-noon");
 const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide");
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
 const dashs = document.querySelector(".dashs");
+
+imgNoon.insertAdjacentHTML(
+  "beforeend",
+  '<img src="images/8.png" alt="img-noon" width="100%">'
+);
 
 let curSlide = 0;
 const maxSlide = slides.length; //Number of Nodelist
