dcb4e45308640e42bec6f8fa4952b9ed7104176c
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-02 14:12:08 +0300

    update

--- script.js
+++ script.js
@@ -147,9 +147,28 @@ const getCategories = async () => {
 //Search
 const searchProducts = async () => {
   const searchValue = document.querySelector(".inpt-search").value;
+  const dropDown = document.querySelector(".dropdown-content");
+
+  if (searchValue == "") {
+    dropDown.innerHTML = "";
+    dropDown.classList.remove("show");
+    return;
+  }
+  dropDown.innerHTML = "";
   const res = await getData(`products/search?q=${searchValue}`);
   console.log(res);
+  res.products.length > 0
+    ? dropDown.classList.add("show")
+    : dropDown.classList.remove("show");
+  res.products.map((product, i) => {
+    if (i >= 10) return;
+    let newOption = document.createElement("a");
+    newOption.textContent = product.title;
+    newOption.href = "#";
+    dropDown.appendChild(newOption);
+  });
 };
+
 // To Get The Id Of a Product When Click On it And open Single Product Page
 const openSingleProductPage = (id) => {
   const date = new Date().getTime() + 3;
